dd2d30675ee39776c07d01602817153121aeac52
LuizFelipeJun <104800410+LuizFelipeJun@users.noreply.github.com>
2022-08-19 19:34:24 -0300

    Add files via upload
    Console.log

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Meu primeiro projeto no console");
+var primeiroNome="Osvaldo ";
+var segundoNome="Tereso";
+var nometodo=primeiroNome.concat(segundoNome);
+console.log(nometodo);
+
+var a=1;
+var b=2;
+var soma=a+b;
+console.log("1+2= "+soma);
+
+var sub=a-b;
+console.log("1-2= "+sub);
+
+var multi=a*b;
+console.log("1x2= "+multi);
+
+var divisao=a/b;
+console.log("1:2= "+divisao);
